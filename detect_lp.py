8f99a330f14e39a03ef4526186599503bf9c3a35
chang-1 <jorge.chang@lytica.ai>
2023-05-09 22:23:18 -0400

    ocr

--- detect_lp.py
+++ detect_lp.py
@@ -59,7 +59,7 @@ if args.source != '0':
                 cv2.putText(cv_img, ' '.join(lp_text), (x1-40, y1-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         # Display annotated image
         # Write the processed image to file
-        cv2.imwrite("/files/{f}.jpg", cv_img)     
+        cv2.imwrite("./files/{f}.jpg", cv_img)     
 
 else:
     # Initialize the webcam
